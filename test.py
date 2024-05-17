d4ba61934f56686c7b50ef9a041c6841d0dd0c48
lizzonator <138684135+lizzonator@users.noreply.github.com>
2024-05-18 01:28:46 +0500

    Update test.py

--- test.py
+++ test.py
@@ -2,40 +2,20 @@ from main import init, fill, show, db, ORDERS, CLIENTS
 from peewee import *
 from os.path import exists
 
-# проверка что база данных появляется после инита
+# проверка что база данных есть
 def test_init():
-    db.connect()
-    #init()
     assert exists("lab1.db")
-    db.close()
 
-# def test_init_second_time():
-#     db.connect()
-#     init()
-#     assert exists("lab1.db")
-#     db.close()
-
-# def test_show_empty():
-#     db.connect()
-#     for tablename in ("clients", "orders"):
-#         show(tablename)
-#     db.close()
+# проверка что есть нужные столбцы
+def test_columns():
+    assert 'NAME' in Clients._meta.fields
+    assert 'CITY' in Clients._meta.fields
+    assert 'ADDRESS' in Clients._meta.fields
+    assert 'CLIENT' in Orders._meta.fields
+    assert 'DATE' in Orders._meta.fields
+    assert 'AMOUNT' in Orders._meta.fields
+    assert 'DESCRIPTION' in Orders._meta.fields
 
 # проверка что в базе данных есть 10 записей
 def test_fill():
-    db.connect()
-    # for i in range(25):
-    #     fill()
     assert CLIENTS.select().count() >= 10
-    db.close()
-
-# def test_show():
-#     db.connect()
-#     for tablename in ("clients", "orders"):
-#         show(tablename)
-#     db.close()
-
-# дописать проверку что после инита появляются нужные колонки
-# def test_columns():
-#     db.connect()
-#     assert 
